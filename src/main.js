3559ad418ab3443fcc96de06aafccc4338cb831c
Tom McQuade <tmac10411@gmail.com>
2021-10-23 18:03:42 -0400

    Status update

--- src/main.js
+++ src/main.js
@@ -3,5 +3,10 @@ function afterLoad(){
     document.getElementById('menu').addEventListener('click',()=>{
         document.getElementById('sidebar').classList.toggle('active');
     });
+
+    const items = document.getElementsByClassName('item');
+    for (let item in items){
+        window.addEventListener('click', )
+    }
 }
 window.addEventListener('load', afterLoad);
