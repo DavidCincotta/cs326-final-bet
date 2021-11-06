65cec4098f23886e1c468129837957cdf103cbf7
David Cincotta <decincotta@gmail.com>
2021-11-06 16:27:59 -0400

    first draft of server code

--- src/database.js
+++ src/database.js
@@ -1,17 +1,31 @@
 import * as fs from 'fs';
 
-function find(table,id){
-    //TODO
+const dbFile = './db.json';
+let db={};
+if(fs.existsSync(dbFile)){
+    db = JSON.parse(fs.readFileSync(stateFile));
+}
+function write(){
+    fs.writeFileSync(dbFile,JSON.stringify(db));
+}
 
+function findId(table,id){
+    return db.table[id];
 }
 
 function findAndUpdate(table,id,obj){
-    //TODO
-
+    if(table in db && id in db.table){
+        db.table[id] = obj;
+        return true;
+    }
+    else: return false;
 }
 
 function insert(table,obj){
     //TODO
     //autopopulate id fields
 
+    let id = db.table.length;
+    db.table.push(obj);
+    return id;
 }
--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
 'use strict';
 import * as http from 'http';
 import * as url from 'url';
-import * as db from './database.js';
+//import * as db from './database.js';
 import express from 'express';
 
-
-
-/////////////////////////////////////////////
-//////////// Forum enpoints ////////////////
 
 const app = express();
 // import express from "express"
@@ -15,6 +11,9 @@ const app = express();
 app.use(express.json()); // lets you handle JSON input
 
 const port = 3010;
+
+/////////////////////////////////////////////
+//////////// Forum enpoints ////////////////
 app.post('/Forum/create', (req, res) =>{
     const course = req.body['course_key'];
     const title = req.body['post_title'];
@@ -24,31 +23,56 @@ app.post('/Forum/create', (req, res) =>{
     // res.send(JSON.stringify(res.statusCode));
     res.send({"course": course, "title": title, "posts": posts})
 });
-
 app.post('/Forum/update', (req, res) => {
     const post = req.body['post_id'];
     const posts = req.body['content_array'];
     // put new info into database WHERE post_id = post_id
     // res.send(JSON.stringify(res.statusCode));
-    res.send({"post": post, "posts": posts})
+    res.send({"post": post, "posts": posts});
 
 });
-
 app.get('/Forum/longpost/:post_id', (req, res) => {
     const postID = req.params.post_id;
     // get and return content_array and post_title, course from db
-    res.send({"title": "title", "posts": ["here's a post", "and another"], "course": "web programming"})
+    res.send({"title": "title", "posts": ["here's a post", "and another"], "course": "web programming"});
 
 });
-
 app.get('/Forum/shortpost/:post_id', (req, res) => {
-    const post = req.params.post_id
+    const post = req.params.post_id;
     // get post title from db using post id
     // res.send({"post_title": post_title})
     // res.send(JSON.stringify(res.statusCode));
-    res.send({"post_id": post})
+    res.send({"post_id": post});
+});
+
+/////////////////////////////////////////////
+//////////// Course enpoints ////////////////
+app.post('/Courses/getcourse', (req, res) =>{
+    const account = req.body['account_id'];
+
+    res.send([{'id':'1','name':'web programming','course_number':'326','description':'learning about front end applications and browsers'},{'id':'2','name':'data structures','course_number':'187','description':'basics of storing and accessing information'},{'id':'3','name':'discrete math','course_number':'...','description':'predicate mathematics and proofing'}]);
+});
+app.get('/Courses/directory', (req, res) =>{
+    res.send([{'id':'1','name':'web programming','course_number':'...','description':'learning about front end applications and browsers'},{'id':'2','name':'data structures','course_number':'...','description':'basics of storing and accessing information'},{'id':'3','name':'discrete math','course_number':'...','description':'predicate mathematics and proofing'},{'id':'1','name':'web programming','course_number':'...','description':'learning about front end applications and browsers'},{'id':'2','name':'data structures','course_number':'...','description':'basics of storing and accessing information'},{'id':'3','name':'discrete math','course_number':'...','description':'predicate mathematics and proofing'}]);
+});
+app.post('/Courses/addcourse', (req, res) =>{
+    res.send();
+});
+app.post('/Courses/search', (req, res) =>{
+    res.send([{'id':'1','college':'CICS','name':'web programming','course_number':'...','description':'learning about front end applications and browsers'},{'id':'2','college':'CICS','name':'data structures','course_number':'...','description':'basics of storing and accessing information'},{'id':'3','college':'CICS','name':'discrete math','course_number':'...','description':'predicate mathematics and proofing'}]);
 
 });
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
