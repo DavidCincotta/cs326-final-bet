1220e1163e971c54a4a8a6a4200ec2233b1d5f0f
David Cincotta <decincotta@gmail.com>
2021-11-06 15:31:22 -0400

    basic struc

--- src/server.js
+++ src/server.js
@@ -14,19 +14,27 @@ server.on('request', async (request, response) => {
     let options = await url.parse(request.url, true).query;
     
     response.writeHead(status, headerText);
-    if (request.url.startsWith("/wordScore")) {
-        postWordScore(options);
-        write();
+    if (request.url.startsWith("/Account/login")) {
     }
-    else if (request.url.startsWith("/highestWordScores")) {
-        getHighestWordScores(options,response);
+    else if (request.url.startsWith("/Account/register")) {
     }
-    else if (request.url.startsWith("/gameScore")) {
-        postGameScore(options);
-        write();
+    else if (request.url.startsWith("/Account/addCourse")) {
     }
-    else if (request.url.startsWith("/highestGameScores")) {
-        getHighestGameScores(options,response);
+    else if (request.url.startsWith("/Courses/getCourses")) {
+    }
+    else if (request.url.startsWith("/Courses/getDirectory")) {
+    }
+    else if (request.url.startsWith("/Courses/search")) {
+    }
+    else if (request.url.startsWith("/Courses/addCourse")) {
+    }
+    else if (request.url.startsWith("/Forum/createPost")) {
+    }
+    else if (request.url.startsWith("/Forum/updatePost")) {
+    }
+    else if (request.url.startsWith("/Forum/post")) {
+    }
+    else if (request.url.startsWith("/Forum/shortpost")) {
     }
     else {
         response.statusCode = 400;
