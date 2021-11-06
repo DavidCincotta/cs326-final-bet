68cbc00d05f7a3e8300756670c5ae6ffc858aa98
Tom McQuade <tmac10411@gmail.com>
2021-11-06 19:28:36 -0400

    finalized endpoints (hopefully)

--- src/server.js
+++ src/server.js
@@ -3,71 +3,49 @@ import * as http from 'http';
 // import * as url from 'url';
 // import * as db from 'database.js';
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 
 /////////////////////////////////////////////
 //////////// Forum enpoints ////////////////
 
 const app = express();
-// import express from "express"
 
-// const path = require('path')
 app.use(express.json())
 app.use(express.static('src'));
 
-// app.use(express.static('public')) // lets you handle JSON input
 
-const port = 3010;
+const port = 3000;
 app.post('/Forum/create', (req, res) =>{
     console.log("HERE")
     const course = req.body['course_key'];
     const title = req.body['post_title'];
     const posts = req.body['content_array'];
     // send info to db
-    // return post_id created in db
-    res.sendFile("./createCourse.html", { root: __dirname })
-    //(JSON.stringify(res.statusCode));
-    // res.send({"course": course, "title": title, "posts": posts})
+    ////// WILL RETURN POST ID FROM DB, FAKE INFO FOR NOW ////////
+    res.send({"course": course, "title": title, "posts": posts})
 });
 
 app.post('/Forum/longpost/:post_id/update', (req, res) => {
-    // const post = req.body['post_id'];
     const post = req.params.post_id;
-
     const posts = req.body['content_array'];
-    // put new info into database WHERE post_id = post_id
-    // res.send(JSON.stringify(res.statusCode));
+    // put new info into database WHERE post_id = post_id (UPDATE)
+    ////// WILL RETURN POST ID FROM DB, FAKE INFO FOR NOW ///////
     res.send({"post": post, "posts": posts})
-
 })
 
 app.get('/Forum/longpost/:post_id', (req, res) => {
     const postID = req.params.post_id;
-    // get and return content_array, post_title, and course from db
+    ///// WILL GET AND RETURN CONTENT_ARRAY, POST_TITLE AND COURSE FROM DB, FAKE DATA FOR NOW //////
     res.send({"title": "title", "course": "web programming", "posts": ["here's a post", "and another"]})
 
 })
 
 app.get('/Forum/shortpost/:post_id', (req, res) => {
     const post = req.params.post_id
-    // get post title from db using post id
-    // res.send({"post_title": post_title})
-    // res.send(JSON.stringify(res.statusCode));
+    ////// WILL GET AND RETURN FORUM POST TITLE AND COURSE FROM DB USING POST_ID ///////
+    ////// FAKE DATA FOR NOW //////
     res.send({"post_id": post})
-
 })
 
-app.get('/test', (req, res) =>{
-    res.sendFile('./test.html',  { root : __dirname})
-})
-
-// app.get('*', (req, res) => {
-//     res.send('NO FOOL, BAD COMMAND');
-//   });
   
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
@@ -75,5 +53,4 @@ app.listen(port, () => {
   
 /////////////////////////////////////////////
 
-// server.listen(8080);
 
