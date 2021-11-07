0c2d18b937e70101bf0c9fcc37c205316401a8a4
ilyavash <Ilya.vash1@gmail.com>
2021-11-06 20:23:00 -0400

    added account login, cretae and setting functions"
    "

--- src/account.js
+++ src/account.js
@@ -1,82 +1,104 @@
+import {postData} from './utilities.js';
 
-async function getLogin(body){
-    const reply = await fetch("http://localhost:3000/account/loginjdskaf", body);
-    console.log(reply)
-    return reply.status;
-}
-
-const loginBtn = document.getElementById('loginBtn');
-const submitBtn = document.getElementById('submitBtn');
-const updateBtn = document.getElementById('updateBtn');
-
-if (loginBtn!== null){
-    loginBtn.addEventListener('click',()=>{
-        const login = document.getElementById('login').value;
-        const password = document.getElementById('password').value;
-        if(!(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(login))){
-            alert("Invalid Email!");
-            return;
-        }
-        const body = {"email": login, "password": password}
-        const x = getLogin(body); 
-        if(x===null){
+function afterLoad(){
+    async function getLogin(body){  
+        const log = await postData('account/login',body);
+        if(log==='account_id'){
             document.location.href = './courses.html';
         }
-    })
-}
-if (submitBtn!== null){
-    submitBtn.addEventListener('click',()=>{
-        const checkbox = document.getElementById('notifCheck').checked;
-        const login = document.getElementById('login').value;
-        const password = document.getElementById('password').value;
-        const password2 = document.getElementById('password2').value;
-        if(!(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(login))){
-            alert("Invalid Email!");
-            return;
+        else{
+            alert("account does not exist");
         }
-        if (password.length<8){
-            alert("Password should be atleast 8 characters!");
-            return;
-        }
-        if(password!==password2){
-            alert("Passwords do not match!");
-            return;
-        }
-
-        document.location.href = './courses.html';
-    })
-}
-if (updateBtn!==null){
-    updateBtn.addEventListener('click',()=>{
-        const currPass = document.getElementById('currPass').value;
-        const updatePass = document.getElementById('updatePass').value;
-        const confirmPass = document.getElementById('confirmPass').value;
-        const notifCheck = document.getElementById('notifCheck').checked;
-
-        let passData = "" //use the one thats stored on Account
-        if(currPass === passData){
-            const emailUpdate = document.getElementById('emailUpdate').value;
-            if (emailUpdate.length>0&&!(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(emailUpdate))){
-                alert("Email not available");
-            }
-            else{
-                //sent email for update
-            }
-            if (updatePass !== confirmPass){
-                alert("Passwords do not match")
-            }
-            else if(updatePass.length<8&&updatePass.length!==0){
-                alert("Password too short")
-            }
-            else if(updatePass.length!==0){
-                //sent password throguh sevrer
-            }
-            //Sent notifcheck to server
+    }
+    async function createAccount(body){  
+        const create = await postData('account/register',body);
+        if(create!==null){
+            document.location.href = './courses.html';
         }
         else{
-            alert("Wrong Password")
+            alert("something went wrong");
         }
-    })
+    }
+    async function updateAccount(body){
+        const x = await postData('account/update',body);
+        
+    }
+
+    const loginBtn = document.getElementById('loginBtn');
+    const submitBtn = document.getElementById('submitBtn');
+    const updateBtn = document.getElementById('updateBtn');
+
+    if (loginBtn!== null){
+        loginBtn.addEventListener('click',()=>{
+            const login = document.getElementById('login').value;
+            const password = document.getElementById('password').value;
+            if(!(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(login))){
+                alert("Invalid Email!");
+                return;
+            }
+            const body = {"email": login, "password": password};
+            getLogin(body); 
+        })
+    }
+    if (submitBtn!== null){
+        submitBtn.addEventListener('click',()=>{
+            const checkbox = document.getElementById('notifCheck').checked;
+            const login = document.getElementById('login').value;
+            const password = document.getElementById('password').value;
+            const password2 = document.getElementById('password2').value;
+            if(!(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(login))){
+                alert("Invalid Email!");
+                return;
+            }
+            if (password.length<8){
+                alert("Password should be atleast 8 characters!");
+                return;
+            }
+            if(password!==password2){
+                alert("Passwords do not match!");
+                return;
+            }
+            const body = {"email": login, "password": password,"notification_flag": checkbox};
+            createAccount(body);
+        })
+    }
+    if (updateBtn!==null){
+        updateBtn.addEventListener('click',()=>{
+            const currPass = document.getElementById('currPass').value;
+            const updatePass = document.getElementById('updatePass').value;
+            const confirmPass = document.getElementById('confirmPass').value;
+            const notifCheck = document.getElementById('notifCheck').checked;
+
+            let passData = "" //use the one thats stored on Account
+            let data={};
+            if(currPass === passData){
+                const emailUpdate = document.getElementById('emailUpdate').value;
+                if (emailUpdate.length>0&&!(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(emailUpdate))){
+                    alert("Email not available");
+                }
+                else if(emailUpdate.length!=0){
+                    data['email']=emailUpdate;
+                }
+                if (updatePass !== confirmPass){
+                    alert("Passwords do not match");
+                }
+                else if(updatePass.length<8&&updatePass.length!==0){
+                    alert("Password too short");
+                }
+                else if(updatePass.length!==0){
+                    data['password']=updatePass;
+                }
+                data['notification_flag']= notifCheck;
+                console.log(data)
+                updateAccount(data);
+            }
+            else{
+                alert("Wrong Password")
+            }
+        })
+    }
 }
+window.addEventListener('load', afterLoad);
+
 
 
--- src/server.js
+++ src/server.js
@@ -1,122 +1,113 @@
 'use strict';
-/*
 import * as http from 'http';
 import * as url from 'url';
-import * as db from './database.js';
-*/
+//import * as db from './database.js';
 import express from 'express';
-//import cors from 'cors'
-// const express = require('express');
-
-
-// const headerText = JSON.stringify({ "Content-Type": "text/html","Transfer-Encoding": "chunked" });
-
-
-// let server = http.createServer();
-// server.on('request', async (request, response) => {
-    
-//     let status = 200;
-//     let options = await url.parse(request.url, true).query;
-    
-//     response.writeHead(status, headerText);
-//     if (request.url.startsWith("/wordScore")) {
-//         postWordScore(options);
-//         write();
-//     }
-//     else if (request.url.startsWith("/highestWordScores")) {
-//         getHighestWordScores(options,response);
-//     }
-//     else if (request.url.startsWith("/gameScore")) {
-//         postGameScore(options);
-//         write();
-//     }
-//     else if (request.url.startsWith("/highestGameScores")) {
-//         getHighestGameScores(options,response);
-//     }
-//     else {
-//         response.statusCode = 400;
-//     }
-//     response.end();
-// });
-
-
-
 
 /////////////////////////////////////////////
-//////////// Forum enpoints ////////////////
+//////////// Express Defini. ////////////////
+/////////////////////////////////////////////
 
 const app = express();
 // import express from "express"
-
 app.use(express.json()); // lets you handle JSON input
-//app.use(cors())
+const port = 3010;
 
-const port = 3000;
-app.post('/Forum/getCourse', (req, res) =>{
+
+/////////////////////////////////////////////
+//////////// ExpressRouting ////////////////
+/////////////////////////////////////////////
+
+app.use(express.static('src'));
+
+app.post('/Forum/create', (req, res) =>{
     const course = req.body['course_key'];
     const title = req.body['post_title'];
     const posts = req.body['content_array'];
-    // database query to get correct info
-    // load forumPost.html with given content
-    res.send(JSON.stringify(res.statusCode));
+    // send info to db
+    // return post_id created in db
+    // res.send(JSON.stringify(res.statusCode));
+    res.send({"course": course, "title": title, "posts": posts})
 });
-
 app.post('/Forum/update', (req, res) => {
     const post = req.body['post_id'];
     const posts = req.body['content_array'];
-    // database query to get correct post stuff
-    // reload forumPost.html with newly updated information
-    res.send(JSON.stringify(res.statusCode));
-})
+    // put new info into database WHERE post_id = post_id
+    // res.send(JSON.stringify(res.statusCode));
+    res.send({"post": post, "posts": posts});
 
-app.get('Forum/post', (req, res) => {
-    const post = req.body['post_id'];
-    // get content_array and post_title from db
-    // res.send({"content_array": content_array, "post_title": post_title} )
-    res.send(JSON.stringify(res.statusCode));
-})
+});
+app.get('/Forum/longpost/:post_id', (req, res) => {
+    const postID = req.params.post_id;
+    // get and return content_array and post_title, course from db
+    res.send({"title": "title", "posts": ["here's a post", "and another"], "course": "web programming"});
 
-app.get('Forum/shortpost', (req, res) => {
-    const post = req.body['post_id'];
+});
+app.get('/Forum/shortpost/:post_id', (req, res) => {
+    const post = req.params.post_id;
     // get post title from db using post id
     // res.send({"post_title": post_title})
-    res.send(JSON.stringify(res.statusCode));
-})
+    // res.send(JSON.stringify(res.statusCode));
+    res.send({"post_id": post});
+});
 
-app.post('/account/register', (req,res) => {
+/////////////////////////////////////////////
+//////////// Course enpoints ////////////////
+/////////////////////////////////////////////
+app.post('/Courses/getcourse', (req, res) =>{
+    const account = req.body['account_id'];
+
+    res.send([{'id':'1','name':'web programming','course_number':'326','description':'learning about front end applications and browsers'},{'id':'2','name':'data structures','course_number':'187','description':'basics of storing and accessing information'},{'id':'3','name':'discrete math','course_number':'...','description':'predicate mathematics and proofing'}]);
+});
+app.get('/Courses/directory', (req, res) =>{
+    res.send([{'id':'1','name':'web programming','course_number':'...','description':'learning about front end applications and browsers'},{'id':'2','name':'data structures','course_number':'...','description':'basics of storing and accessing information'},{'id':'3','name':'discrete math','course_number':'...','description':'predicate mathematics and proofing'},{'id':'1','name':'web programming','course_number':'...','description':'learning about front end applications and browsers'},{'id':'2','name':'data structures','course_number':'...','description':'basics of storing and accessing information'},{'id':'3','name':'discrete math','course_number':'...','description':'predicate mathematics and proofing'}]);
+});
+app.post('/Courses/addcourse', (req, res) =>{
+    res.send();
+});
+app.post('/Courses/search', (req, res) =>{
+    res.send([{'id':'1','college':'CICS','name':'web programming','course_number':'...','description':'learning about front end applications and browsers'},{'id':'2','college':'CICS','name':'data structures','course_number':'...','description':'basics of storing and accessing information'},{'id':'3','college':'CICS','name':'discrete math','course_number':'...','description':'predicate mathematics and proofing'}]);
+
+});
+
+/////////////////////////////////////////////
+//////////// Account endpoints ////////////////
+/////////////////////////////////////////////
+app.post('/Account/register', (req,res) => {
     const account = {
         username: req.body.username,
         email: req.body.email,
         password: req.body.password,
         notification_flag: req.body.notification_flag
     };
-    //store account 
-    res.send(JSON.stringify(res.statusCode))
+    //store account in db
+    //sent account to client
+    res.send(JSON.stringify(account))
 })
-app.post('/account/login', (req,res)=> {
+app.post('/Account/login', (req,res)=> {
     const email = req.body['email'];
-    console.log(email)
     const password = req.body['password'];
-
-    res.send(JSON.stringify(res.statusCode));
+    //check if match with data base
+    //then sent id
+    res.send(JSON.stringify("account_id"));
 })
-app.post('/account/addcourse',(req,res)=>{
-
+app.post('/Account/addcourse',(req,res)=>{
+    const account = req.body['account_id'];
+    const course = req.body['course'];
+    res.send([{'id':'1','college':'CICS','name':'web programming','course_number':'...','description':'learning about front end applications and browsers'},{'id':'2','college':'CICS','name':'data structures','course_number':'...','description':'basics of storing and accessing information'},{'id':'3','college':'CICS','name':'discrete math','course_number':'...','description':'predicate mathematics and proofing'}]);
 })
-
+app.post('/Account/update',(req,res)=>{
+    //update account settings from body in db
+    
+    res.send(JSON.stringify(res.body));
+})
+/*
 app.get('*', (req, res) => {
     res.send('NO FOOL, BAD COMMAND');
   });
-  
-app.listen(port);
-  
-/////////////////////////////////////////////
-
-// server.listen(8080);
-
-/*
-
+*/
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
-*/
+  
+/////////////////////////////////////////////
